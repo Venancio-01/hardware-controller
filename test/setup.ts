48dff00ea7e9441dcb8bb46eef41e7c1cf91887c
vaausud <15054854614@163.com>
2025-12-20 15:55:53 +0800

    chore: setup bun test environment

---
+++ test/setup.ts
@@ -0,0 +1,10 @@
+import { beforeAll, afterAll } from "bun:test";
+
+beforeAll(() => {
+  // Global setup
+  console.log("Global test setup initialized");
+});
+
+afterAll(() => {
+  // Global teardown
+});
